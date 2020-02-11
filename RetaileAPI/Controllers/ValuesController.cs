0f9bb24d74a93a2e17305b5b3244850150aff27a
Hex-Zero <hex0@live.com>
2020-02-11 21:30:14 +0000

    user id return

--- RetaileAPI/Controllers/ValuesController.cs
+++ RetaileAPI/Controllers/ValuesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -13,7 +14,8 @@ namespace RetaileAPI.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            string userId = RequestContext.Principal.Identity.GetUserId();
+            return new string[] { "value1", "value2", userId };
         }
 
         // GET api/values/5
