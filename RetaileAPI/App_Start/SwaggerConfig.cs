da009e4de065204d42da4ab59631db296a8e0df7
Hex-Zero <hex0@live.com>
2020-02-12 21:37:32 +0000

    Swagger set up finished

--- RetaileAPI/App_Start/SwaggerConfig.cs
+++ RetaileAPI/App_Start/SwaggerConfig.cs
@@ -2,6 +2,7 @@ using System.Web.Http;
 using WebActivatorEx;
 using RetaileAPI;
 using Swashbuckle.Application;
+using RetaileAPI.App_Start;
 
 [assembly: PreApplicationStartMethod(typeof(SwaggerConfig), "Register")]
 
@@ -16,7 +17,8 @@ namespace RetaileAPI
             GlobalConfiguration.Configuration
                 .EnableSwagger(c =>
                     {
-
+                        c.DocumentFilter<AuthTokenOperation>();
+                        c.OperationFilter<AuthorizationOperationFilter>();
                         // By default, the service root url is inferred from the request used to access the docs.
                         // However, there may be situations (e.g. proxy and load-balanced environments) where this does not
                         // resolve correctly. You can workaround this by providing your own code to determine the root URL.
