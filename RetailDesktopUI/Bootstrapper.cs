0e140f99b0b3521e44aa3810324850c252122bac
Hex-Zero <hex0@live.com>
2020-02-13 16:54:00 +0000

    Login data gets passed donw

--- RetailDesktopUI/Bootstrapper.cs
+++ RetailDesktopUI/Bootstrapper.cs
@@ -1,4 +1,5 @@
 ﻿using Caliburn.Micro;
+using RetailDesktopUI.Helpers;
 using RetailDesktopUI.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace RetailDesktopUI
 {
@@ -15,6 +17,12 @@ namespace RetailDesktopUI
         public Bootstrapper()
         {
             Initialize();
+
+
+            ConventionManager.AddElementConvention<PasswordBox>(
+                PasswordBoxHelper.BoundPasswordProperty,
+                "Password",
+                "PasswordChanged");
         }
         protected override void Configure()
         {
---
+++ RetailDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,46 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RetailDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+		private string _userName;
+		private string _password;
+		public string UserName
+		{
+			get { return _userName; }
+			set 
+			{ 
+				_userName = value;
+				NotifyOfPropertyChange(() => UserName);
+			}
+		}
+		public string Password
+		{
+			get { return _password; }
+			set
+			{ 
+				_password = value;
+				NotifyOfPropertyChange(() => Password);
+			}
+		}
+		public bool CanLogIn(string userName, string password)
+		{
+			bool output = false;
+			if(userName?.Length > 0 && Password?.Length > 0)
+			{
+				output = true;
+			}
+			return output;
+		}
+		public void  LogIn(string userName, string password)
+		{
+			Console.WriteLine();
+		}
+	}
+}
