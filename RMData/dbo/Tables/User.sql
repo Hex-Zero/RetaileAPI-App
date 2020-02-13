d395279c8e633e9815b963ecf3c7a2b9b12169c0
Hex-Zero <hex0@live.com>
2020-02-13 15:39:45 +0000

    set up SQL tables

---
+++ RMData/dbo/Tables/User.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] NVARCHAR(128) NOT NULL PRIMARY KEY, 
+    [FirstName] NVARCHAR(50) NULL, 
+    [LastName] NVARCHAR(50) NULL, 
+    [EmailAddress] NVARCHAR(256) NULL, 
+    [CreatedDate] DATETIME2 NULL DEFAULT getutcdate()
+)
