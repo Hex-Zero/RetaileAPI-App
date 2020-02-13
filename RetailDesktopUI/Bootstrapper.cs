e6b79d74c01bb2034502fdaad601875a6999e342
Hex-Zero <hex0@live.com>
2020-02-13 14:51:30 +0000

    Bootstrapper

--- RetailDesktopUI/Bootstrapper.cs
+++ RetailDesktopUI/Bootstrapper.cs
@@ -11,13 +11,39 @@ namespace RetailDesktopUI
 {
     class Bootstrapper : BootstrapperBase
     {
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
         }
+        protected override void Configure()
+        {
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+
+            GetType().Assembly.GetTypes().Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewnModelType => _container.RegisterPerRequest(
+                    viewnModelType, viewnModelType.ToString(), viewnModelType));
+        }
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
             DisplayRootViewFor<ShellViewModel>();
+        }
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
         }
     }
 }
