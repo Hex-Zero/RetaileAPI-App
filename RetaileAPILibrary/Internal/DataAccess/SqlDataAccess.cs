b90340625ab31b202b34bdf3a00538e456e1a7d8
Hex-Zero <hex0@live.com>
2020-02-20 16:32:26 +0000

    Fix the data base exeption

--- RetaileAPILibrary/Internal/DataAccess/SqlDataAccess.cs
+++ RetaileAPILibrary/Internal/DataAccess/SqlDataAccess.cs
@@ -47,6 +47,7 @@ namespace RetaileAPILibrary.Internal.DataAccess
             _connection = new SqlConnection(connectionString);
             _connection.Open();
             _transaction = _connection.BeginTransaction();
+            isClosed = false;
         }
         public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
         {
@@ -60,20 +61,38 @@ namespace RetaileAPILibrary.Internal.DataAccess
 
                 return rows;
         }
+        private bool isClosed = false;
         public void CommitTransaction()
         {
             _transaction?.Commit();
             _connection?.Close();
+
+            isClosed = true;
         }
         public void RollbackTransaction()
         {
             _transaction?.Rollback();
             _connection?.Close();
+            isClosed = true;
+
         }
 
         public void Dispose()
         {
-            CommitTransaction();
+
+            if (isClosed == false)
+            {
+                try
+                {
+                    CommitTransaction();
+                }
+                catch 
+                {
+                    // TODO - Log this issue
+                }
+            }
+            _transaction = null;
+            _connection = null;
         }
     }
 }
