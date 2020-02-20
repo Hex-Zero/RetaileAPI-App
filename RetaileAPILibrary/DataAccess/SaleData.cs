da63c29ab25f39d6188ac76f3043cdb17f23e93f
Hex-Zero <hex0@live.com>
2020-02-20 13:34:01 +0000

    Open sql connection with c#

--- RetaileAPILibrary/DataAccess/SaleData.cs
+++ RetaileAPILibrary/DataAccess/SaleData.cs
@@ -45,15 +45,27 @@ namespace RetaileAPILibrary.DataAccess
             };
             sale.Total = sale.SubTotal + sale.Tax;
 
-            SqlDataAccess sql = new SqlDataAccess();
-            sql.SaveData<SaleDBModel>("dbo.spSale_Insert", sale, "RMData");
-
-            sale.Id = sql.LoadData<int, dynamic>("spSale_Lookup", new { sale.CashierID, sale.SaleDate },"RMData").FirstOrDefault();
-
-            foreach (var item in details)
+            using (SqlDataAccess sql = new SqlDataAccess())
             {
-                item.SaleId = sale.Id;
-                sql.SaveData("dbo.spSaleDetail_Insert", item, "RMData");
+                try
+                {
+                    sql.StartTransaction("RMData");
+                    sql.SaveDataInTransaction<SaleDBModel>("dbo.spSale_Insert", sale);
+
+                    sale.Id = sql.LoadDataInTransaction<int, dynamic>("spSale_Lookup", new { sale.CashierID, sale.SaleDate }).FirstOrDefault();
+
+                    foreach (var item in details)
+                    {
+                        item.SaleId = sale.Id;
+                        sql.SaveDataInTransaction("dbo.spSaleDetail_Insert", item);
+                    }
+                    sql.CommitTransaction();
+                }
+                catch 
+                {
+                    sql.RollbackTransaction();
+                    throw;
+                }
             }
         }
     }
--- RetaileAPILibrary/Internal/DataAccess/SqlDataAccess.cs
+++ RetaileAPILibrary/Internal/DataAccess/SqlDataAccess.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 
 namespace RetaileAPILibrary.Internal.DataAccess
 {
-    internal class SqlDataAccess
+    internal class SqlDataAccess : IDisposable
     {
         public string GetConnectionString(string name)
         {
@@ -37,6 +37,43 @@ namespace RetaileAPILibrary.Internal.DataAccess
                 connection.Execute(storedProcedure, parameters,
                     commandType: CommandType.StoredProcedure);
             }
+        }
+        private IDbConnection _connection;
+        private IDbTransaction _transaction;
+        public void StartTransaction(string connectionStringName)
+        {
+            string connectionString = GetConnectionString(connectionStringName);
+
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+            _transaction = _connection.BeginTransaction();
+        }
+        public void SaveDataInTransaction<T>(string storedProcedure, T parameters)
+        {
+                _connection.Execute(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure, transaction: _transaction);
+        }
+        public List<T> LoadDataInTransaction<T, U>(string storedProcedure, U parameters)
+        {
+                List<T> rows = _connection.Query<T>(storedProcedure, parameters,
+                    commandType: CommandType.StoredProcedure,transaction: _transaction).ToList();
+
+                return rows;
+        }
+        public void CommitTransaction()
+        {
+            _transaction?.Commit();
+            _connection?.Close();
+        }
+        public void RollbackTransaction()
+        {
+            _transaction?.Rollback();
+            _connection?.Close();
+        }
+
+        public void Dispose()
+        {
+            CommitTransaction();
         }
     }
 }
